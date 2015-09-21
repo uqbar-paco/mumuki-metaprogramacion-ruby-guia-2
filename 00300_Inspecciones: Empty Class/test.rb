6f7bc3e930b3b57ed1e7b505200105231e345c65
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-09-21 16:09:41 -0300

    Fixing test

--- 00300_Inspecciones: Empty Class/test.rb
+++ 00300_Inspecciones: Empty Class/test.rb
@@ -12,8 +12,16 @@ describe "Inspections" do
   let(:inspector) { Inspector.new }
 
   describe('empty_class?') do
-    it "Object no es empty class" do
-     expect(inspector.empty_class? Object).to be false
+    it "Object es empty class" do
+     expect(inspector.empty_class? Object).to be true
+    end
+
+    it "String no es empty class" do
+     expect(inspector.empty_class? String).to be false
+    end
+
+    it "Class no es empty class" do
+     expect(inspector.empty_class? Class).to be false
     end
 
     it "(class Foo; end) es empty class" do
