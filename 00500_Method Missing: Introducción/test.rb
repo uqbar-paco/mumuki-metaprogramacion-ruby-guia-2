6ddb5d40dc9b83aebea102376845f488daa0ba7e
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-09-23 19:33:45 -0300

    Corrigiendo descripcion de tests

--- 00500_Method Missing: Introducción/test.rb
+++ 00500_Method Missing: Introducción/test.rb
@@ -2,7 +2,7 @@ describe Hodor do
   let(:hodor) { Hodor.new }
 
   it("hodor.foo! == \"Hodor!\"") { expect(hodor.foo!).to eq "Hodor!" }
-  it("hodor.foo! :bar == \"Hodor!\"") { expect(hodor.foo! :bar).to eq "Hodor! Hodor!" }
-  it("hodor.foo!(:baz) { } == \"Hodor!\"") { expect(hodor.foo!(:baz) { }).to eq "Hodor! Hodor!" }
-  it("hodor.foo! :bar, :baz == \"Hodor!\"") { expect(hodor.foo! :bar, :baz).to eq "Hodor! Hodor! Hodor!" }
+  it("hodor.foo! :bar == \"Hodor! Hodor!\"") { expect(hodor.foo! :bar).to eq "Hodor! Hodor!" }
+  it("hodor.foo!(:baz) { } == \"Hodor! Hodor!\"") { expect(hodor.foo!(:baz) { }).to eq "Hodor! Hodor!" }
+  it("hodor.foo! :bar, :baz == \"Hodor! Hodor! Hodor!\"") { expect(hodor.foo! :bar, :baz).to eq "Hodor! Hodor! Hodor!" }
 end
