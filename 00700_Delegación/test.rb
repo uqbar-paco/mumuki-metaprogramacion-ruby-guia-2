9ca6b4c99affdb9573e0d64ef0ad0d5e4d2b8d76
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-09-23 23:21:02 -0300

    Mejorando mensajes de error

--- 00700_Delegación/test.rb
+++ 00700_Delegación/test.rb
@@ -15,8 +15,15 @@ describe Forwarder do
   end
   let(:baz) { OpenStruct.new(foo: 2, bar: 4, baz:5) }
 
-  it { expect(Foo.new(baz).baz).to eq 5 }
-  it { expect(Foo.new(baz).bar).to eq "bar" }
-  it { expect(Foo.new(baz).baz).to eq 5 }
-  it { expect { Foo.new(nil).baz }.to raise NoMethodError }
+  it("si el delegador tiene el método, no delega") do
+    expect(Foo.new(baz).bar).to eq "bar"
+  end
+
+  it("si el delegador no tiene el método, y el delegado entiende el mensaje, delega") do
+   expect(Foo.new(baz).baz).to eq 5
+  end
+
+  it("si el delagador no tiene el método y el delegado no entiende el mensaje, lanza NoMethodError") do
+   expect { Foo.new(nil).baz }.to raise_error NoMethodError
+  end
 end
