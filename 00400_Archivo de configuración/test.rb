d0165ead5fe762d59a1e63feb1a22946918bac4a
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-09-28 10:55:46 -0300

    Corrigiendo test

--- 00400_Archivo de configuración/test.rb
+++ 00400_Archivo de configuración/test.rb
@@ -5,18 +5,18 @@ describe "config" do
   end
 
   it "Configuration.new(notification_mode: :sms).read_notification_mode devuelve un SmsNotificationMode" do
-    expect(read(notification_mode: :sms)).to be_a SmsNotificationMode
+    expect(read(notification_mode: "sms")).to be_a SmsNotificationMode
   end
 
   it "Configuration.new(notification_mode: :facebook).read_notification_mode devuelve un FacebookNotificationMode" do
-    expect(read(notification_mode: :facebook)).to be_a FacebookNotificationMode
+    expect(read(notification_mode: "facebook")).to be_a FacebookNotificationMode
   end
 
   it "Configuration.new(notification_mode: :email).read_notification_mode devuelve un EmailNotificationMode" do
-    expect(read(notification_mode: :email)).to be_a EmailNotificationMode
+    expect(read(notification_mode: "email")).to be_a EmailNotificationMode
   end
 
   it "Configuration.new(notification_mode: :pepita).read_notification_mode lanza ConfigurationError" do
-    expect { read(notification_mode: :pepita) }.to raise_error(ConfigurationError)
+    expect { read(notification_mode: "pepita") }.to raise_error(ConfigurationError)
   end
 end
