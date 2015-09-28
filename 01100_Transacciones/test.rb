c07d96ba161763b783fc98e0acda028de5896209
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-09-28 14:21:38 -0300

    Corrigiendo error de sintaxis en tests

--- 01100_Transacciones/test.rb
+++ 01100_Transacciones/test.rb
@@ -11,23 +11,23 @@ describe "transaction" do
 
   it "un metodo definido como transactional devuelve lo que retorna el bloque" do
     class Baz < TransactionalObject
-       transactional :foo { 5 }
+       transactional(:foo) { 5 }
     end
     expect(Baz.new.foo).to eq 5
   end
 
   context "si el metodo no falla" do
     class Foo < TransactionalObject
-       transactional :foo {}
+       transactional(:foo) {}
     end
     before { Bar.new.foo }
-    it "comitea" {  expect(Transaction.committed).to be true  }
-    it "no hace rollback" { expect(Transaction.rollbacked).to be false }
+    it("comitea") {  expect(Transaction.committed).to be true  }
+    it("no hace rollback") { expect(Transaction.rollbacked).to be false }
   end
 
   context "si el metodo falla" do
     class Bar < TransactionalObject
-       transactional :foo { raise "error" }
+       transactional(:foo) { raise "error" }
     end
     it "hace rollback" do
       expect { Bar.new.foo }.to raise_error("error")
