099d8c99610ba2750787104d029af9b17556ff48
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-09-28 13:43:17 -0300

    Typo

--- 00825_Macros, segunda parte/test.rb
+++ 00825_Macros, segunda parte/test.rb
@@ -14,15 +14,15 @@ describe "ensure_no_override" do
       end
     end
     class Foo; ensure_no_override :bar; end
-    expect(Foo.responds_to? :bar).to be tue
+    expect(Foo.respond_to? :bar).to be tue
   end
 
   it "no falla si la clase no existia" do
     class Bar; ensure_no_override :bar; end
-    expect(Foo.responds_to? :bar).to be true
+    expect(Foo.respond_to? :bar).to be true
   end
 
   it "lo entienden los modulos" do
-    expect(Module.responds_to? :ensure_no_override).to be true
+    expect(Module.respond_to? :ensure_no_override).to be true
   end
 end
